a04b425215eeac876490fca2c85d719a8cbd3754
lukeskywalker16 <luke.reid1997@gmail.com>
2025-10-27 16:26:33 +1100

    Change mouse position tracking to client coordinates

--- trail.js
+++ trail.js
@@ -1,8 +1,8 @@
 document.addEventListener("mousemove", (e) => {
   const trail = document.createElement("div");
   trail.className = "trail";
-  trail.style.left = `${e.pageX}px`;
-  trail.style.top = `${e.pageY}px`;
+  trail.style.left = `${e.clientX}px`;
+  trail.style.top = `${e.clientY}px`;
   document.body.appendChild(trail);
 
   setTimeout(() => {
