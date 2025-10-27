7411e14dd998150a044407ea8ea34f3cc2816fd6
lukeskywalker16 <luke.reid1997@gmail.com>
2025-10-28 09:31:20 +1100

    Update trail.js

--- trail.js
+++ trail.js
@@ -1,10 +1,11 @@
+
 document.addEventListener("mousemove", (e) => {
   const trail = document.createElement("div");
   trail.className = "trail";
   trail.style.left = `${e.clientX}px`;
   trail.style.top = `${e.clientY}px`;
   document.body.appendChild(trail);
-
+  
   setTimeout(() => {
     trail.remove();
   }, 1000);
