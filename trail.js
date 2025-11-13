28a4543c4d0613aa6a1d3a7cca4f8458d757428d
lukeskywalker16 <luke.reid1997@gmail.com>
2025-11-14 01:13:36 +1100

    Update trail.js

--- trail.js
+++ trail.js
@@ -1,5 +1,5 @@
-
- {"  >  const trail = document.createElement("div");
+document.addEventListener("mousemove", (e) => {
+  const trail = document.createElement("div");
   trail.className = "trail";
   trail.style.left = `${e.clientX}px`;
   trail.style.top = `${e.clientY}px`;
